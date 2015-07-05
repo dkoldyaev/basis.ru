9606a88823ef906b1eb1ed0065c2db607bb25a5d
dkoldyaev <dkoldyaev@gmail.com>
2015-07-05 15:28:52 +0500

    исправлена работа меню с подразделами

--- menu/context_processors.py
+++ menu/context_processors.py
@@ -7,13 +7,20 @@ def menu_items(request) :
     try:    current_header_menu_item = HeaderMenuItem.objects.get_active(path__iexact=request.path, hide_not_active=not request.user.is_staff)
     except: current_header_menu_item = None
 
-    try:    parent_header_menu_item =  HeaderMenuItem.objects.get_active(path__istartswith=request.path, hide_not_active=not request.user.is_staff)
-    except: parent_header_menu_item =  None
+    parent_header_menu_items =  HeaderMenuItem.objects.none()
+    path = request.path
+
+    while (len(path) > 1 and parent_header_menu_items.count() == 0) :
+        try:
+            path = '/'.join(path.split('/')[:-1])
+            parent_header_menu_items |= HeaderMenuItem.objects.filter_active(path__iexact=path, hide_not_active=not request.user.is_staff)
+        except:
+            pass
 
     return {
 
         'header_menu_items':        HeaderMenuItem.objects.all_active(hide_not_active=not request.user.is_staff),
         'current_header_menu_item': current_header_menu_item,
-        'parent_header_menu_item':  parent_header_menu_item,
+        'parent_header_menu_items': parent_header_menu_items,
 
     }
