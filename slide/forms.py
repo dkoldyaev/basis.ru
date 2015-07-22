c2b6720db158cfc587436ac0c9cdee963ef414a3
dkoldyaev <ololo!!11>
2015-07-22 15:02:15 +0500

    Бага с формой для слайдов

--- slide/forms.py
+++ slide/forms.py
@@ -3,7 +3,7 @@ __author__ = u'Кольдяев Дмитрий'
 
 from django import forms
 from django.contrib import admin
-from suit.widgets import NumberInput, EnclosedInput
+from suit.widgets import EnclosedInput
 
 class SlideForm(forms.ModelForm):
 
@@ -12,11 +12,11 @@ class SlideForm(forms.ModelForm):
             'title_width':      EnclosedInput(append='px', attrs={'class': 'input-mini'}),
             'title_digit':      EnclosedInput(attrs={'class': 'input-mini'}),
 
-            'title_line1_deg':  NumberInput(attrs={'class': 'input-mini'}),
-            'title_line1_top':  NumberInput(attrs={'class': 'input-mini'}),
-            'title_line1_right':NumberInput(attrs={'class': 'input-mini'}),
+            'title_line1_deg':  EnclosedInput(attrs={'class': 'input-mini'}),
+            'title_line1_top':  EnclosedInput(attrs={'class': 'input-mini'}),
+            'title_line1_right':EnclosedInput(attrs={'class': 'input-mini'}),
 
-            'title_line2_deg':  NumberInput(attrs={'class': 'input-mini'}),
-            'title_line2_top':  NumberInput(attrs={'class': 'input-mini'}),
-            'title_line2_right':NumberInput(attrs={'class': 'input-mini'}),
+            'title_line2_deg':  EnclosedInput(attrs={'class': 'input-mini'}),
+            'title_line2_top':  EnclosedInput(attrs={'class': 'input-mini'}),
+            'title_line2_right':EnclosedInput(attrs={'class': 'input-mini'}),
         }
