26c53530c6c8d0f77a193253568e2112b35932c6
dkoldyaev <ololo!!11>
2015-07-22 14:47:28 +0500

    Включен дебаг для ветки dev

--- basis/settings.py
+++ basis/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r#41%wh8q4+amq+v)rcn&il)1k9pll-(6tqm!tj&%@#4@&ior='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bazis.u28010.netangels.ru', 'bazis66.ru']
 
